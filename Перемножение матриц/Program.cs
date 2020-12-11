39e5cea79073635b997f9ddf3bcf8fe1ba33edfe
iksiks <iksiks30@gmail.com>
2020-12-11 22:31:24 +0300

    Создан новый проект Перемножение матриц

---
+++ Перемножение матриц/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Перемножение_матриц
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
