33e2de3cee81a8fbd3b3a5fcbcc5643910873b9e
iksiks <iksiks30@gmail.com>
2020-12-13 22:48:44 +0300

    проект Медленные слова готов

--- Медленные слова/Program.cs
+++ Медленные слова/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Linq;
 
-namespace Медленные_слова
+namespace Slow_Word
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static string Word(string slowWord)
+		{
+			char[] letters = slowWord.ToCharArray();
+			char[] phrase = new char[letters.Length];
+			int j = 0;
+			phrase[0] = letters[0];
+
+			for (int i = 1; i < letters.Length; i++)
+			{
+				if (letters[i] != phrase[j])
+				{
+					j++;
+					phrase[j] = letters[i];
+				}
+			}
+
+			string s = new string(phrase, 0, j + 1);
+			return s;
+		}
+
+		static void Main(string[] args)
+		{
+			string s = Word("пппппппрррииииивввееееееттттт        ввввсссссееееммммм уууучччащщщщиммммссссяяяяя");
+			Console.Write(s);
+			Console.ReadKey();
+		}
+	}
 }
