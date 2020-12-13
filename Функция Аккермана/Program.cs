4a96a748db2de98dc55d6f35be7d4777785e5605
iksiks <iksiks30@gmail.com>
2020-12-14 00:29:54 +0300

    Функция Аккермана готова

--- Функция Аккермана/Program.cs
+++ Функция Аккермана/Program.cs
@@ -4,9 +4,34 @@ namespace Функция_Аккермана
 {
     class Program
     {
+        static int Akkernan(int n, int m)
+        {
+            if (n == 0) return m + 1;
+            else if (m == 0 && n != 0) return Akkernan(n - 1, 1);
+            else return Akkernan(n - 1, Akkernan(n, m - 1));            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 0, m = 0;            
+            for (int i = 1; i <= 2; i++)
+            {                
+                Console.Write($"Введите {i} число ");                
+                bool result = int.TryParse(Console.ReadLine(), out int number);
+                while (!result || number < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"Вводите только числа(положительные): ");
+                    Console.ResetColor();
+                    result = int.TryParse(Console.ReadLine(), out number);
+                }
+                if (i == 1)
+                {
+                    n = number;
+                }
+                else m = number;
+            }                       
+            Console.WriteLine($"Значение функции Аккермана равно {Akkernan(n, m)}");
+            Console.ReadKey();
         }
     }
 }
