7bcdcec28935e642e8c1c11adff529e2acc96d1d
iksiks <iksiks30@gmail.com>
2020-12-13 22:15:22 +0300

    новый проект Медленные слова

---
+++ Медленные слова/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Медленные_слова
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
