7906d1c793b8d54b38c794d6702a6b38d7539f64
iksiks <iksiks30@gmail.com>
2020-12-13 23:23:16 +0300

    Новый проект Функция_Аккермана

---
+++ Функция Аккермана/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Функция_Аккермана
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
