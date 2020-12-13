d9992d38a27eb3f6c6a7427fc80dd03dd52cd0f0
iksiks <iksiks30@gmail.com>
2020-12-14 00:32:19 +0300

    Поменял название функции

--- Функция Аккермана/Program.cs
+++ Функция Аккермана/Program.cs
@@ -4,11 +4,11 @@ namespace Функция_Аккермана
 {
     class Program
     {
-        static int Akkernan(int n, int m)
+        static int Akkerman(int n, int m)
         {
             if (n == 0) return m + 1;
-            else if (m == 0 && n != 0) return Akkernan(n - 1, 1);
-            else return Akkernan(n - 1, Akkernan(n, m - 1));            
+            else if (m == 0 && n != 0) return Akkerman(n - 1, 1);
+            else return Akkerman(n - 1, Akkerman(n, m - 1));            
         }
         static void Main(string[] args)
         {
@@ -30,7 +30,7 @@ namespace Функция_Аккермана
                 }
                 else m = number;
             }                       
-            Console.WriteLine($"Значение функции Аккермана равно {Akkernan(n, m)}");
+            Console.WriteLine($"Значение функции Аккермана равно {Akkerman(n, m)}");
             Console.ReadKey();
         }
     }
